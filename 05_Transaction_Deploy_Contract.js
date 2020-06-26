b7149d1a324408738dd280e5b824a5900497b394
Abid Jaweed <52130668+Abidjaweed@users.noreply.github.com>
2020-06-26 23:44:03 +0500

    Create 05_Transaction_Deploy_Contract.js

---
+++ 05_Transaction_Deploy_Contract.js
@@ -0,0 +1,34 @@
+var Tx = require('ethereumjs-tx')
+const Web3 = require('web3')
+const web3 = new Web3('https://ropsten.infura.io/v3/4d121ccc6c334599bcae2743738e14fb');
+
+const account1 = "0x27484D261F02bf2910B022c5273Dcb52A552eEa7"; // acc 1 of my MetaMask wallet
+const privateKey1 = "99BEDBC1527D51C3DB5BCEB467CE0259F11484683BAE84A7E115CD808CDCFEFC"; // Private key Acc 1
+const privateKey1Buffer = Buffer.from(privateKey1, 'hex');
+
+//console.log("Buffer 1 = ",privateKey1Buffer);
+
+web3.eth.getTransactionCount(account1, (err, txCount)=>{
+    const data = "0x608060405234801561001057600080fd5b5061019b806100206000396000f3fe608060405234801561001057600080fd5b50600436106100415760003560e01c8063303b519214610046578063967e6e65146100c9578063d5dcf127146100e7575b600080fd5b61004e610115565b6040518080602001828103825283818151815260200191508051906020019080838360005b8381101561008e578082015181840152602081019050610073565b50505050905090810190601f1680156100bb5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b6100d1610152565b6040518082815260200191505060405180910390f35b610113600480360360208110156100fd57600080fd5b810190808035906020019092919050505061015b565b005b60606040518060400160405280601681526020017f446f20536f6d6520576f726b20666f722068756d616e00000000000000000000815250905090565b60008054905090565b806000819055505056fea26469706673582212204874b284b5cb5ac9f1b40861780d64a6be25228c7815c1bfc568f12da849f1b464736f6c63430006060033"
+    const txObject = {
+        nonce:    web3.utils.toHex(txCount),
+        gasLimit: web3.utils.toHex(800000),
+        gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+        data: data
+      }
+
+    const tx = new Tx.Transaction(txObject, { chain: 'ropsten' });
+    tx.sign(privateKey1Buffer);
+
+    const serializedTx = tx.serialize();
+    const raw = '0x' + serializedTx.toString('hex');
+
+    web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+        console.log('err:', err);
+        console.log('txHash:', txHash)
+        
+    });
+
+});
+
+
